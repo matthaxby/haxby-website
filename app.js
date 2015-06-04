933e301685ec9a84cfcdb349e4ac32660de63076
Matt Haxby <matthaxby@gmail.com>
2015-06-03 18:51:19 -0600

    added a fade-in for colors in header

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var layerTwo = document.getElementById("layer-two");
 var path = document.querySelector('path');
 var length = path.getTotalLength();
 
+
 path.style.transition = path.style.WebkitTransition =  'none';
 
 path.style.strokeDasharray = length + ' ' + length;
@@ -16,15 +17,16 @@ path.style.transition = path.style.WebkitTransition =
   'stroke-dashoffset 2s ease-in-out';
 
 path.style.strokeDashoffset = '0';
-//
-// var turnOff = function (bg) {
-//   bg.style.display = "none", 2000;
-// };
-//
-// var turnOn = function (bg) {
-//   bg.style.display = "block", 2000;
-// };
-//
-//
-// setTimeout(turnOff(layerOne), 2000);
-// setTimeout(turnOn(layerTwo), 2000);
+
+var turnOff = function () {
+  layerOne.style.display = "none";
+};
+
+var turnOn = function () {
+  layerTwo.style.opacity = "1";
+  layerTwo.style.transition = "opacity .25s ease-in-out"
+};
+
+
+setTimeout(turnOff, 2000);
+setTimeout(turnOn, 800);
