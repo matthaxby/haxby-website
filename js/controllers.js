022601ea5a37c112808f6173dccd0edda4c82f8b
Matt Haxby <matthaxby@gmail.com>
2015-09-07 10:05:45 -0600

    starting to work on routes and dropdown

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,16 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.projects = [
     {image: "images/Ghost.png", title: "Ghost", description: "Ghost is a word game where 2 players take turns adding letters to an unfinished word. A player wins when the other player either completes a word or adds a letter that can’t be a word.", technical: "Google OAuth, Express, Handlebars, Mongo, Node, SASS and the tricky parsing of a library of 27,000 words."},
-    {image: "images/Orc-Fight.png", title: "Orc Fight", description: "A side project while learning Javascript, Orc Fight! simulates a fight between two characters that use different moves at random until one character wins.", technical: "Javascript, HTML, CSS, Multi-Media, recursion"},
+    {image: "images/Orc-Fight.png", title: "Orc Fight!", description: "A side project while learning Javascript, Orc Fight! simulates a fight between two characters that use different moves at random until one character wins.", technical: "Javascript, HTML, CSS, Multi-Media, recursion"},
     {image: "images/Reddit-Clone.png", title: "Reddit Clone", description: "The Reddit Clone was an exercise in making single page apps using Angular. A user can create posts, as well as comment and vote.", technical: "Angular, SASS, injected a relative date module"}
   ]
-
+  $scope.menuToggle = false
   $scope.currentIndex = 0
   $scope.direction = 'left'
 
+  $scope.toggleMenu = function() {
+    $scope.menuToggle = !$scope.menuToggle
+  }
   $scope.setCurrentProjectIndex = function (index) {
     $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right'
     $scope.currentIndex = index
---
+++ js/routes.js
@@ -0,0 +1,15 @@
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/home.html'
+    })
+    .when('/projects', {
+      templateUrl: 'templates/projects.html'
+    })
+    .when('/about', {
+      templateUrl: 'templates/about.html'
+    })
+    .when('/contact', {
+      templateUrl: 'templates/contact.html'
+    })
+}])
